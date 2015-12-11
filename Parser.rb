b6488adc01656ed2f85aed4eb7434a755ce1490c
Chris Guard & Jessie Richardson <pair+cguard90.jesskrich@devbootcamp.com>
2015-12-11 16:08:31 -0500

    Fix bug, To_s method began, code is cooperating

--- Parser.rb
+++ Parser.rb
@@ -2,7 +2,7 @@ module Parser
 
   def read(file_name)
     flashcards = []
-    File.foreach(file_name) do |thing|
+    File.foreach(file_name) do |card|
       flashcards << card.chomp
     end
     flashcards.delete_if{ |file_string| file_string == "" }
