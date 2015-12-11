d4a3db73257cab77e1d9f440cb88436bb3d205fc
Chris Guard & Jessie Richardson <pair+cguard90.jesskrich@devbootcamp.com>
2015-12-11 14:48:07 -0500

    Module Complete.

---
+++ Parser.rb
@@ -0,0 +1,13 @@
+module Parser
+
+  def read(file_name)
+    flashcards = []
+    File.foreach(file_name) do |card|
+      flashcards << card.chomp
+    end
+    flashcards.delete_if{ |file_string| file_string == "" }
+  end
+
+end
+
+p read('flash_cards.txt')
