5c1e6c696337eddaaf4639cbe6b59c278ccb4f7c
Chris Guard & Jessie Richardson <pair+cguard90.jesskrich@devbootcamp.com>
2015-12-11 15:04:32 -0500

    Remove test line

--- Parser.rb
+++ Parser.rb
@@ -2,7 +2,7 @@ module Parser
 
   def read(file_name)
     flashcards = []
-    File.foreach(file_name) do |card|
+    File.foreach(file_name) do |thing|
       flashcards << card.chomp
     end
     flashcards.delete_if{ |file_string| file_string == "" }
@@ -10,4 +10,4 @@ module Parser
 
 end
 
-p read('flash_cards.txt')
+
